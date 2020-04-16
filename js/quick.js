7331e628a836d1b24035251281b5e523fd1a0fb2
swd762 <ilyakazalin@yandex.ru>
2020-04-16 21:38:50 +0400

    some experiments

---
+++ js/quick.js
@@ -0,0 +1,30 @@
+// // ** script for cards sizing
+//
+// $('.prod').each(function(){
+//
+//     $(this).hover(function () {
+//          // tmpHeight = getTotalHeigh($(this));
+//
+//         $(this).animate({
+//             'height': totalHeight + 50 }, 200);
+//         }, function () {
+//             // var th = getTotalHeigh($(this));
+//             $(this).animate({
+//                 'height': totalHeight }
+//             );
+//             // tmpHeight = 0;
+//     });
+//
+//
+//
+// });
+//
+// function getTotalHeigh(obj)
+// {
+//     var totalHeight = 0;
+//     obj.each(function () {
+//         totalHeight += $(this).height();
+//     });
+//     console.log(totalHeight);
+//     return totalHeight;
+// }
