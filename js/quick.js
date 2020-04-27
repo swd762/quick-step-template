6829833c1b653d870b16c9837687d350d7df6bbf
swd762 <ilyakazalin@yandex.ru>
2020-04-27 21:34:21 +0400

    some experiments with mobile menu

--- js/quick.js
+++ js/quick.js
@@ -39,15 +39,18 @@ if ($(window).width() <= 900) {
 
     })
 $menus = $('.mobile-menu');
-$('.has-children').click(function (e) {
+$('.has-children').on('click', '.has-children', function (e) {
     console.log($menus);
     e.preventDefault();
+    // e.stopPropagation();
+
     // $('.mobile-menu .sub-menu' ).toggleClass('active');
     $('.mobile-menu .sub-menu__content').addClass('active');
 
 
 
-})
+
+});
 
     // $menus = $('.mobile-menu>.has-children');
 // for (let i = 0; i <$menus.length ; i++) {
