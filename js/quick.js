f82de8e7b38a246311d4338c777c9c456c08fcc9
swd762 <ilyakazalin@yandex.ru>
2020-04-28 14:14:23 +0400

    update links to lazy load

--- js/quick.js
+++ js/quick.js
@@ -1,9 +1,7 @@
+
 // disable hover on cards in mobile width
 let cards = $('.product-item');
-// console.log(cards);
-
 if ($(window).width() <= 900) {
-
     for (let i = 0; i <cards.length ; i++) {
         cards[i].classList.remove('desktop--hover');
     }
@@ -40,7 +38,7 @@ if ($(window).width() <= 900) {
     })
 $menus = $('.mobile-menu');
 $('.has-children').click(function (e) {
-    e.preventDefault();
+    // e.preventDefault();
     $(this).next('.child').children('.sub-menu__content').toggleClass('active');
 });
 $('.sub-menu__content .title-link').click(function (e) {
