06b8cfc40cf8ae994e5fd078b87cebaf5dcc254a
swd762 <ilyakazalin@yandex.ru>
2020-05-06 08:21:17 +0400

    update mob menu

--- js/quick.js
+++ js/quick.js
@@ -12,24 +12,29 @@ if ($(window).width() <= 900) {
 // ** ** **
 
 // ** searchbar activate mobile
-    $('.navbar-mobile__search').bind('click', function (e) {
-        e.preventDefault();
-        $('.navbar-mobile__wrapper').toggleClass('is-search');
-        $('.header-mobile').toggleClass('is-search');
-        $('.search-bar').toggleClass('hidden');
-    })
-
+$('.navbar-mobile__search').bind('click', function (e) {
+    e.preventDefault();
+    $('.navbar-mobile__wrapper').toggleClass('is-search');
+    $('.header-mobile').toggleClass('is-search');
+    $('.search-bar').toggleClass('hidden');
+});
 // ** ** **
 
-    $('.navbar-mobile__hamburger').bind('click',function (e) {
-        e.preventDefault();
-        $(this).toggleClass('expanded-menu');
-        $('.mobile-menu').slideToggle(300, function () {
-            $('.mobile-menu').toggleClass('active');
-        })
-    })
+// ** mob menu activete
+$('.navbar-mobile__hamburger').bind('click',function (e) {
+    e.preventDefault();
+    $(this).toggleClass('expanded-menu');
+    $('.mobile-menu').slideToggle(300, function () {
+        $('.mobile-menu').toggleClass('active');
 
-$menus = $('.mobile-menu');
+        $('.navbar-mobile__search').toggle();
+        $('.navbar-mobile__tel').toggle();
+        $('.navbar-mobile__cart').toggle();
+    })
+});
+// ** ** **
+
+// ** activate sub menus in mobile
 $('.has-children').click(function (e) {
     // e.preventDefault();
     $(this).next('.child').children('.sub-menu__content').toggleClass('active');
@@ -38,17 +43,15 @@ $('.sub-menu__content .title-link').click(function (e) {
     e.preventDefault();
     $(this).parent().parent().toggleClass('active');
 });
-
+// ** ** **
 
 
 // ** extra in stock page
-
 $('.stock-card__extra-link').click(function (e) {
         e.preventDefault();
         $(this).toggleClass('extend');
         $(this).parent().next('.stock-card__main-extra').slideToggle('active');
 });
-
 // ******
 
 // ** more link in account **
@@ -56,8 +59,8 @@ $('.account__order-history-card-more').click(function (e) {
         e.preventDefault();
         $(this).next().slideToggle('active');
 })
-
 // ******
+
 
 // Инициализация b-lazy
 var bLazy = new Blazy();
