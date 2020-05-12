b92ac80d9d509b67d0cc0bf6e9d08cd03804d06f
swd762 <ilyakazalin@yandex.ru>
2020-05-12 16:45:17 +0400

    some work with delivery

--- js/quick.js
+++ js/quick.js
@@ -85,7 +85,7 @@ $(".slowly").on("click", function (event) {
     event.preventDefault();
     var id = $(this).attr('href'),
         top = $(id).offset().top;
-    $('body,html').animate({scrollTop: top}, 1000);
+    $('body,html').animate({scrollTop: top}, 100);
 });
 // ***
 
